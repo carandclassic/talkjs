f117418741be4441ffebfd2b54b129f679d4c294
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2021-07-07 14:47:32 +0100

    Laravel Binding error - add and cleanup the ServiceProvider so that it throws appropriate error

--- src/Providers/TalkJSServiceProvider.php
+++ src/Providers/TalkJSServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
 
 namespace CarAndClassic\TalkJS\Providers;
 
 use CarAndClassic\TalkJS\TalkJSClient;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
 
 class TalkJSServiceProvider extends ServiceProvider
@@ -11,18 +13,33 @@ class TalkJSServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../../config/talkjs.php', 'talkjs'
+            __DIR__ . '/../../config/talkjs.php',
+            'talkjs'
         );
-        $this->app->bind(TalkJSClient::class, static function($app, $params) {
-            $appId = $params['appId'] ?? config('talkjs.app_id');
-            $secretKey = $params['secretKey'] ?? config('talkjs.secret_key');
-            return new TalkJSClient($appId, $secretKey);
-        });
+
+        $this->app->bind(
+            TalkJSClient::class,
+            static function ($app, $params) {
+                $appId = $params['appId'] ?? config('talkjs.app_id');
+                $secretKey = $params['secretKey'] ?? config('talkjs.secret_key');
+
+                if ($appId === null || $secretKey === null) {
+                    throw new BindingResolutionException(
+                        'Cannot create TalkJs Client - either $appId or $secretKey were not populated'
+                    );
+                }
+
+                return new TalkJSClient($appId, $secretKey);
+            }
+        );
     }
+
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__ . "/../../config/talkjs.php" => config_path('talkjs.php')
-        ]);
+        $this->publishes(
+            [
+                __DIR__ . "/../../config/talkjs.php" => config_path('talkjs.php')
+            ]
+        );
     }
 }
