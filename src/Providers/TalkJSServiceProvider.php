f0a246309903fff5b7c21ba29638126c1c223c96
Nikolaos Spyratos <nik@ankyr.dev>
2021-07-07 12:08:14 +0200

    Add Laravel ServiceProvider register and boot, add config publishing

---
+++ src/Providers/TalkJSServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace CarAndClassic\TalkJS\Providers;
+
+use CarAndClassic\TalkJS\TalkJSClient;
+use Illuminate\Support\ServiceProvider;
+
+class TalkJSServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(TalkJSClient::class, static function($app, $params) {
+            $appId = $params['appId'] ?? config('talkjs.app_id');
+            $secretKey = $params['secretKey'] ?? config('talkjs.secret_key');
+            return new TalkJSClient($appId, $secretKey);
+        });
+    }
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . "/../../config/talkjs.php" => config_path('talkjs.php')
+        ]);
+    }
+}
